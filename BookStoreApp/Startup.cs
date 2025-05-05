0464a4e64f1de1f98448acc1613e319bf80096e2
Disha <dishakamble24@gmail.com>
2025-05-06 00:07:06 +0530

    [Disha] ADD: DB Configuration, user migration

--- BookStoreApp/Startup.cs
+++ BookStoreApp/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RepositoryLayer.Context;
 
 namespace BookStoreApp
 {
@@ -26,6 +28,9 @@ namespace BookStoreApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //for configure database connection
+            services.AddDbContext<BookDBContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DbConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ RepositoryLayer/Context/BookDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Context
+{
+    public class BookDBContext : DbContext
+    {
+        public BookDBContext(DbContextOptions option) : base(option)  {  }
+
+        //For create User table 
+        public DbSet<UserEntity> Users { get; set; }
+
+    }
+}
