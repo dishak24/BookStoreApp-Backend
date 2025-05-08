b1760e7d9b4ef4f6dadc8cdd739c88c4d1fc3c39
Disha <dishakamble24@gmail.com>
2025-05-09 02:07:43 +0530

    [Disha] Created: BooksContext created using scaffold

---
+++ RepositoryLayer/Context/BooksContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using RepositoryLayer.Entity;
+
+namespace RepositoryLayer.Context
+{
+    public partial class BooksContext : DbContext
+    {
+        public BooksContext()
+        {
+        }
+
+        public BooksContext(DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Books> Books { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Server=LAPTOP-T927S6BH\\SQLEXPRESS;Database=BookStoreDB;Trusted_Connection=True;");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Books>(entity =>
+            {
+                entity.HasKey(e => e.BookId);
+
+                entity.Property(e => e.BookId).ValueGeneratedNever();
+
+                entity.Property(e => e.Author)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.BookImage).HasMaxLength(300);
+
+                entity.Property(e => e.BookName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(750);
+
+                entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
