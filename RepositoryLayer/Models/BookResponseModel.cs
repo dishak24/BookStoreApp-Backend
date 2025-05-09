2d109317a6479910fbadbfc230273cb6f6c3c9bf
Disha <dishakamble24@gmail.com>
2025-05-10 02:39:11 +0530

    [Disha] Modified: added BookId to display in response

--- RepositoryLayer/Models/BookResponseModel.cs
+++ RepositoryLayer/Models/BookResponseModel.cs
@@ -6,6 +6,7 @@ namespace RepositoryLayer.Models
 {
     public class BookResponseModel
     {
+        public int BookId { get; set; }
         public string BookName { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
