c0ce89b552458c2d05145e4236002e8ffe806cf3
Disha <dishakamble24@gmail.com>
2025-05-10 00:35:48 +0530

    [Disha] Added: Book Response Model to get proper response

---
+++ RepositoryLayer/Models/BookResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Models
+{
+    public class BookResponseModel
+    {
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int DiscountPrice { get; set; }
+        public int Quantity { get; set; }
+        public string BookImage { get; set; }
+        
+    }
+}
