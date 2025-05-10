65f86a43a975e4d284df59dc4152e734d950fb98
Disha <dishakamble24@gmail.com>
2025-05-11 02:45:40 +0530

    [Disha] Modified: email id added for display after login

--- RepositoryLayer/Models/LoginResponseModel.cs
+++ RepositoryLayer/Models/LoginResponseModel.cs
@@ -8,7 +8,7 @@ namespace RepositoryLayer.Models
     {
         public string Name { get; set; }
 
-        //public string Email { get; set; }
+        public string Email { get; set; }
         public string AccessToken { get; set; }
         public string RefreshToken { get; set; }
         //public int UserId { get; set; }
