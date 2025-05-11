d537dc502d12fe5af04db78a3fc31e47d75a623e
Disha <dishakamble24@gmail.com>
2025-05-11 16:58:02 +0530

    [Disha] Added: response model for carts

---
+++ RepositoryLayer/Models/CartResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Models
+{
+    public class CartResponseModel
+    {        
+        public int CartId { get; set; }
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public int TotalPrice => UnitPrice * Quantity;
+        public string BookImage { get; set; }
+        
+
+    }
+}
