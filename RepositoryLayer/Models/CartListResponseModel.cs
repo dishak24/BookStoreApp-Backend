33890ced1f401ae561dd79b60084bc1d584403ae
Disha <dishakamble24@gmail.com>
2025-05-12 18:05:19 +0530

    [Disha] Added: CartListResponseModel for calculate TotalAmount from cart

---
+++ RepositoryLayer/Models/CartListResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Models
+{
+    public class CartListResponseModel
+    {
+        public List<CartResponseModel> Items { get; set; }
+        public int TotalAmount { get; set; }
+    }
+}
