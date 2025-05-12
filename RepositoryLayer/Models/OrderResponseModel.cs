9ed3a758f57323250b9f660917b654efc54d728e
Disha <dishakamble24@gmail.com>
2025-05-13 02:38:01 +0530

    [Disha] Added: response for order books data

---
+++ RepositoryLayer/Models/OrderResponseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Models
+{
+    public class OrderResponseModel
+    {
+        
+        public int OrderId { get; set; }
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public int Quantity { get; set; }
+        public int TotalAmount { get; set; }
+        public string BookImage { get; set; }
+        public DateTime OrderedAt { get; set; }
+        
+
+    }
+}
