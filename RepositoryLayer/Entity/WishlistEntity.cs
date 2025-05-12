2443ca71905f1567e7c2bfc71fb15288ab7376fc
Disha <dishakamble24@gmail.com>
2025-05-12 19:38:39 +0530

    [Disha] Added: wishlist entity

---
+++ RepositoryLayer/Entity/WishlistEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RepositoryLayer.Entity
+{
+    public class WishlistEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int WishlistId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int BookId { get; set; }
+
+        public DateTime AddedAt { get; set; } = DateTime.Now;
+
+        // Navigation properties
+        [ForeignKey("UserId")]
+        public virtual UserEntity User { get; set; }
+
+        [ForeignKey("BookId")]
+        public virtual Books Book { get; set; }
+    }
+}
