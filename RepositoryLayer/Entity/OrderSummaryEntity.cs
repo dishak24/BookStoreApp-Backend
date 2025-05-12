0be4a5edb9f14db64f1d2b97bf198b8e23601076
Disha <dishakamble24@gmail.com>
2025-05-13 02:04:50 +0530

    [Disha] Added: Order Summary entity

---
+++ RepositoryLayer/Entity/OrderSummaryEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class OrderSummaryEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [ForeignKey("Book")]
+        public int BookId { get; set; }
+
+        public int Quantity { get; set; }
+        public int TotalAmount { get; set; }
+        public DateTime OrderedAt { get; set; } = DateTime.Now;
+
+        // Navigation properties
+        public virtual UserEntity User { get; set; }
+        public virtual Books Book { get; set; }
+    }
+
+}
