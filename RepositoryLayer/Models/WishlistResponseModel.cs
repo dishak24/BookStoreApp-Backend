e5e68380ba30178070d54c345cc6bd1a0e769843
Disha <dishakamble24@gmail.com>
2025-05-12 22:46:03 +0530

    [Disha] Added: response model for wishlist

---
+++ RepositoryLayer/Models/WishlistResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Models
+{
+        public class WishlistResponseModel
+        {
+            public int WishlistId { get; set; }
+            public int BookId { get; set; }
+            public string BookName { get; set; }
+            public string Author { get; set; }
+            public string BookImage { get; set; }
+            public int Price { get; set; }
+            public int DiscountPrice { get; set; }
+       
+        }
+
+    
+}
