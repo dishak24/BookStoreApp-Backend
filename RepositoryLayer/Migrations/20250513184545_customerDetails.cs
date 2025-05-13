f019d8750c89953e3fb6339581fc81cb33c2ff2e
Disha <dishakamble24@gmail.com>
2025-05-14 01:07:32 +0530

    [Disha] Added: Migration for customer Details

---
+++ RepositoryLayer/Migrations/20250513184545_customerDetails.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class customerDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerDetails",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(nullable: false),
+                    FullName = table.Column<string>(nullable: true),
+                    Mobile = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    UserEntityUserId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerDetails", x => x.CustomerId);
+                    table.ForeignKey(
+                        name: "FK_CustomerDetails_Users_UserEntityUserId",
+                        column: x => x.UserEntityUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CustomerDetails_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerDetails_UserEntityUserId",
+                table: "CustomerDetails",
+                column: "UserEntityUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerDetails_UserId",
+                table: "CustomerDetails",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerDetails");
+        }
+    }
+}
