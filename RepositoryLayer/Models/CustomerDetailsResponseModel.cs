ce8c20768f3201aad5b3cbbad46f4d77c543c7b8
Disha <dishakamble24@gmail.com>
2025-05-14 01:11:37 +0530

    [Disha] Added: customer Details response model

---
+++ RepositoryLayer/Models/CustomerDetailsResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Models
+{
+    public class CustomerDetailsResponseModel
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Type { get; set; }
+    }
+}
