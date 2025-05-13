ed4e4db68ca8f9d63e70743e6e4fe005a3dc5c29
Disha <dishakamble24@gmail.com>
2025-05-14 01:11:05 +0530

    [Disha] Added: customer Details DTO/model

---
+++ RepositoryLayer/Models/CustomerDetailsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Models
+{
+    //(for POST request)
+    public class CustomerDetailsModel
+    {
+        public string FullName { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Type { get; set; }
+    }
+
+}
