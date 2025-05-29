65e44132f3c8ea40b2332dc7cad6b43d5c7f74d5
Disha <dishakamble24@gmail.com>
2025-05-30 01:05:49 +0530

    [Disha] Added: for get all carts using SP

---
+++ RepositoryLayer/Models/CartViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace RepositoryLayer.Models
+{
+    public class CartViewModel
+    {
+        public int CartId { get; set; }
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public string BookImage { get; set; }
+        public bool IsPurchased { get; set; }
+    }
+
+}
